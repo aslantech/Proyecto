c2f3094049ed63d748a9417c7f8a96fc58237234
Sanchez_DiegoLeo <38506689+SanchezLeo@users.noreply.github.com>
2018-05-29 09:17:10 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php 
+
+include 'inc/comun.php';
+//Datos de configuraci&oacute;n para header
+
+	
+
+$bd = new GestarBD;    
+include 'inc/head.php';
+include 'inc/header.php';
+
+ 
+
+include 'inc/opciones.php';
+
+include 'inc/finhtml.php';
---
+++ indexadmin.php
@@ -0,0 +1,14 @@
+<?php 
+
+include 'inc/comun.php';
+
+$bd = new GestarBD;    
+include 'inc/head.php';
+include 'inc/header2.php';
+
+ 
+
+include 'inc/opciones2.php';
+
+
+include 'inc/finhtml.php';
